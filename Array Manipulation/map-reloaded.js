0e4c8ca51c7a72263704e0ddce7da7a04985b50d
Jaime Alonso <jaimea111@gmail.com>
2023-11-08 20:37:28 -0500

    Adding map reloaded

---
+++ Array Manipulation/map-reloaded.js
@@ -0,0 +1,27 @@
+﻿const orders = [
+    {
+        customerName: 'Nicolas',
+        total: '60',
+        delivered: false
+    },
+    {
+        customerName: 'Jaime',
+        total: '80',
+        delivered: true
+    },
+    {
+        customerName: 'Javier',
+        total: '76',
+        delivered: false
+    },
+    {
+        customerName: 'Pedro',
+        total: '51',
+        delivered: true
+    },
+    {
+        customerName: 'Carlos',
+        total: '12',
+        delivered: false
+    },
+];
